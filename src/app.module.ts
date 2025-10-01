0dabfc0e865bc6139ea8a2a076fccb5ad51a3af7
Benjamin Sonnet <rainlandsociety@gmail.com>
2025-10-01 17:22:11 +0100

    Initial cat CRUD commit.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CatsModule } from './cats/cats.module';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Mark global to make available to all modules.
+      expandVariables: true, // Expand variables to allow use of variables within the env file.
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`, // Use the .env file based on the NODE_ENV var set in package.json.
+    }),
+    // Initialise the DB connection, using a factory to load the connection string from env vars.
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get('MONGO_CONNECTION_STRING'),
+      }),
+    }),
+    CatsModule,
+  ],
 })
 export class AppModule {}
---
+++ src/cats/cats.service.ts
@@ -0,0 +1,37 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Cat } from './schemas/cat.schema';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+
+@Injectable()
+export class CatsService {
+  constructor(@InjectModel(Cat.name) private catModel: Model<Cat>) {}
+
+  async create(createCatDto: CreateCatDto): Promise<Cat> {
+    const createdCat = await this.catModel.create(createCatDto);
+    return createdCat;
+  }
+
+  async findAll(): Promise<Cat[]> {
+    return this.catModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Cat> {
+    return this.catModel.findOne({ _id: id }).exec();
+  }
+
+  async update(id: string, updateCatDto: UpdateCatDto): Promise<Cat> {
+    return this.catModel
+      .findByIdAndUpdate({ _id: id }, updateCatDto, { new: true })
+      .exec();
+  }
+
+  async delete(id: string): Promise<Cat> {
+    const deletedCat = await this.catModel
+      .findByIdAndDelete({ _id: id })
+      .exec();
+    return deletedCat;
+  }
+}
